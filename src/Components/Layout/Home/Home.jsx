3e8908f91e3c920dc1e1f8f640288f090a38007c
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2025-02-17 15:47:23 +0600

    DATA DIPLAYED

--- src/Components/Layout/Home/Home.jsx
+++ src/Components/Layout/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
+import Books from '../Books/Books'
 
 function Home() {
   return (
     <div> 
 
        <Banner/>
+       <Books/>
 
         
 
